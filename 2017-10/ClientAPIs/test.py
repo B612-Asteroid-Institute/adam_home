5bb7b2dfb0ad1f186386ed21b821c1ab11176e57
Samira Motiwala <samotiwala@alumni.stanford.edu>
2017-12-17 14:42:23 -0800

    comment on test.py

--- 2017-10/ClientAPIs/test.py
+++ 2017-10/ClientAPIs/test.py
@@ -14,7 +14,7 @@ batch_run.set_end_time('2017-10-11T00:00:00Z')
 batch_run.set_state_vector('2017-10-04T00:00:00.000Z', state_vec)
 
 # Optional parameters
-# batch_run.set_propagator_uuid("00000000-0000-0000-0000-000000000002")
+# batch_run.set_propagator_uuid("00000000-0000-0000-0000-000000000002")    # Only Sun as point mass, nothing else
 
 print("Initial state %s" % batch_run)
 print
