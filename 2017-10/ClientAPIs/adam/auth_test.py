ad4bb5f7d745c604d1de2940b395f6a1d79ec104
Laura Lark <laurahlark@gmail.com>
2018-01-11 12:03:54 -0500

    Add tests for auth.py, clean up auth.py a bit.

--- 2017-10/ClientAPIs/adam/auth_test.py
+++ 2017-10/ClientAPIs/adam/auth_test.py
@@ -1,6 +1,7 @@
 
 from adam.auth import Auth
 from adam.rest_proxy import _RestProxyForTest
+import json
 import unittest
 
 class AuthTest(unittest.TestCase):
@@ -9,39 +10,111 @@ class AuthTest(unittest.TestCase):
     """
 
     def setUp(self):
-        """"Set up base URL
-
-        This function sets up the base URL to a dummy URL.
-
-        """
         self._base = "http://BASE"
         
-
-    def test_whoami_anonymous(self):
-        """Test whoami
-
-        This function tests that the whoami check functions as expected.
-
-        """
+    def test_successful_authorization(self):
+        auth = Auth(self._base)
 
         # Use REST proxy for testing
         rest = _RestProxyForTest()
-
-        token = 'some_token'
-
-        # Set expected 'GET' request with 404 error
-        rest.expect_get(self._base + '/me/' + token, 404, {})
-
-        # Initiate Auth class
-        auth = Auth(self._base, token)
-
-        # Override network access with proxy
         auth.set_rest_accessor(rest)
+        
+        # Before authorizing, auth should reflect not logged in.
+        self.assertEqual(auth.get_token(), '')
+        self.assertEqual(auth.get_user(), '')
+        self.assertEqual(auth.get_logged_in(), False)
+        
+        # A successful authorization should store token and set user to returned value.
+        good_token = 'good'
+        rest.expect_get(self._base + '/me?token=' + good_token, 200,
+        	{'email': 'a@b.com', 'loggedIn': True})
+        auth.authorize(good_token)
+        self.assertEqual(auth.get_token(), good_token)
+        self.assertEqual(auth.get_user(), 'a@b.com')
+        self.assertEqual(auth.get_logged_in(), True)
+        
+	def test_unsuccessful_authorization(self):
+		auth = Auth(self._base)
 
-        # Request whoami
+		# Use REST proxy for testing
+		rest = _RestProxyForTest()
+		auth.set_rest_accessor(rest)
+        
+        # Authorize in order to fill in email/logged_in/token so that next test
+        # can verify that these are cleared.
+        rest.expect_get(self._base + '/me?token=' + good_token, 200,
+        	{'email': 'a@b.com', 'loggedIn': True})
+        auth.authorize(good_token)
+		
+        # An unsuccessful authorization should clear token and other values.
+        bad_token = 'bad'
+        # An example of the few ways that the server might reject a user. Others look
+        # like this with different messages.
+        server_error_on_bad_token = """
+			{
+			  "error": {
+				"errors": [
+				  {
+					"domain": "global",
+					"reason": "backendError",
+					"message": "org.apache.shiro.authc.IncorrectCredentialsException"
+				  }
+				],
+				"code": 503,
+				"message": "org.apache.shiro.authc.IncorrectCredentialsException"
+			  }
+			}
+			"""
+        rest.expect_get(self._base + '/me?token=' + bad_token, 200,
+        	json.loads(server_error_on_bad_token))
+        auth.authorize(bad_token)
+        self.assertEqual(auth.get_token(), '')
+        self.assertEqual(auth.get_user(), '')
+        self.assertEqual(auth.get_logged_in(), False)
+        
+	def test_authorization_empty_token(self):
+		auth = Auth(self._base)
+
+		# Use REST proxy for testing
+		rest = _RestProxyForTest()
+		auth.set_rest_accessor(rest)
+        
+        # Authorize in order to fill in email/logged_in/token so that next test
+        # can verify that these are cleared.
+        rest.expect_get(self._base + '/me?token=' + good_token, 200,
+        	{'email': 'a@b.com', 'loggedIn': True})
+        auth.authorize(good_token)
+        
+        # Authorization with an empty token should be no problem and result in an empty
+        # auth object.
+        rest.expect_get(self._base + '/me', 200, {"loggedIn": False})
+        auth.authorize('')
+        self.assertEqual(auth.get_token(), '')
+        self.assertEqual(auth.get_user(), '')
+        self.assertEqual(auth.get_logged_in(), False)
+        
+	def test_authorization_server_error(self):
+		auth = Auth(self._base)
+
+		# Use REST proxy for testing
+		rest = _RestProxyForTest()
+		auth.set_rest_accessor(rest)
+        
+        # Authorize in order to fill in email/logged_in/token so that next test
+        # can verify that these are not cleared.
+        rest.expect_get(self._base + '/me?token=' + good_token, 200,
+        	{'email': 'a@b.com', 'loggedIn': True})
+        auth.authorize(good_token)
+        
+        # Authorization should throw on a non-200 response and leave auth contents
+        # unchanged.
+        rest.expect_get(self._base + '/me?token=problematic_token', 201, {})
         with self.assertRaises(RuntimeError):
-            auth.whoami()
-        
-        rest.expect_get(self._base + '/me/' + token, 200, {'logged_in': False})
-        
-        auth.whoami()
+            auth.authorize('problematic_token')
+        self.assertEqual(auth.get_token(), good_token)
+        self.assertEqual(auth.get_user(), 'a@b.com')
+        self.assertEqual(auth.get_logged_in(), True)
+
+if __name__ == '__main__':
+    unittest.main()
+    	
