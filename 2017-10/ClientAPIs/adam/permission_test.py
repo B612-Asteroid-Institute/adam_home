4cc2886b4b7b3f1908f7d4639bd50e4f0de32e3c
Laura Lark <laurahlark@gmail.com>
2018-02-15 16:44:18 -0500

    Add unit and integration tests of permissions management. Finish permissions implementation.

--- 2017-10/ClientAPIs/adam/permission_test.py
+++ 2017-10/ClientAPIs/adam/permission_test.py
@@ -1,157 +1,131 @@
-from adam import Groups
-from adam.group import Group
-from adam.group import GroupMember
+from adam import Permission
+from adam import Permissions
 from adam.rest_proxy import _RestProxyForTest
 import unittest
 
-class GroupTest(unittest.TestCase):
-    """Unit tests for group object
+class PermissionTest(unittest.TestCase):
+    """Unit tests for permission object
 
     """
     
     def test_get_methods(self):
-        group = Group("uuid", None, None)
-        self.assertEqual("uuid", group.get_uuid())
-        self.assertEqual(None, group.get_name())
-        self.assertEqual(None, group.get_description())
-        
-        group = Group("uuid","name", "description")
-        self.assertEqual("uuid", group.get_uuid())
-        self.assertEqual("name", group.get_name())
-        self.assertEqual("description", group.get_description())
+        permission = Permission("GRANT_READ", "SETTINGS", "uuid")
+        self.assertEqual("GRANT_READ", permission.get_right())
+        self.assertEqual("SETTINGS", permission.get_target_type())
+        self.assertEqual("uuid", permission.get_target_uuid())
 
-class GroupMemberTest(unittest.TestCase):
-    """Unit tests for group member object
+    def test_bad_inputs(self):
+        with self.assertRaises(KeyError):
+            Permission(None, "PROJECT", "id")
+        with self.assertRaises(KeyError):
+            Permission("Not a right", "PROJECT", "id")
+        with self.assertRaises(KeyError):
+            Permission("Admin", "PROJECT", "id")  # Case sensitive
+        with self.assertRaises(KeyError):
+            Permission("ADMIN", None, "id")
+        with self.assertRaises(KeyError):
+            Permission("ADMIN", "not a target type", "id")
+        with self.assertRaises(KeyError):
+            Permission("ADMIN", "pROJECT", "id")  # Case sensitive
+        with self.assertRaises(KeyError):
+            Permission("ADMIN", "PROJECT", None)  # Case sensitive
+
+class PermissionsTest(unittest.TestCase):
+    """Unit tests for permissions module
 
     """
     
-    def test_get_methods(self):
-        member = GroupMember("id", "USER")
-        self.assertEqual("id", member.get_id())
-        self.assertEqual("USER", member.get_type())
-
-class GroupsTest(unittest.TestCase):
-    """Unit tests for groups module
-
-    """
-    
-    def test_new_group(self):
+    def test_grant_user_permission(self):
         rest = _RestProxyForTest()
-        groups = Groups(rest)
+        permissions = Permissions(rest)
 
         expected_data = {}
         def check_input(data_dict):
             self.assertEqual(expected_data, data_dict)
             return True
-            
-        expected_data = {'name': None, 'description': None}
-        rest.expect_post("/group", check_input, 200, {'uuid': 'aaa'})
-        group = groups.new_group(None, None)
-        self.assertEqual("aaa", group.get_uuid())
-        self.assertEqual(None, group.get_name())
-        self.assertEqual(None, group.get_description())
-            
-        expected_data = {'name': 'nnn', 'description': 'ddd'}
-        rest.expect_post("/group", check_input, 200, {'uuid': 'ccc'})
-        group = groups.new_group('nnn', 'ddd')
-        self.assertEqual("ccc", group.get_uuid())
-        self.assertEqual('nnn', group.get_name())
-        self.assertEqual('ddd', group.get_description())
         
+        expected_data = {'right': 'ADMIN', 'target_type': 'PROJECT', 'target_id': 'id'}
+        rest.expect_post('/user_permission/u1', check_input, 200, expected_data)
         
-        rest.expect_post("/group", check_input, 404, {})
+        p1 = Permission('ADMIN', 'PROJECT', 'id')
+        permissions.grant_user_permission('u1', p1)
+        
+        rest.expect_post('/user_permission/u1', check_input, 404, {})
         with self.assertRaises(RuntimeError):
-            group = groups.new_group('nnn', 'ddd')
-        
-    def test_delete_group(self):
-        rest = _RestProxyForTest()
-        groups = Groups(rest)
-        
-        rest.expect_delete("/group/aaa", 204)
-        groups.delete_group('aaa')
-        
-        # 200 isn't a valid return value for delete calls right now
-        rest.expect_delete("/group/aaa", 200)
-        with self.assertRaises(RuntimeError):
-            groups.delete_group('aaa')
-            
-        rest.expect_delete("/group/aaa", 404)
-        with self.assertRaises(RuntimeError):
-            groups.delete_group('aaa')
+            permissions.grant_user_permission('u1', p1)
     
-    def test_add_group_member(self):
+    def test_grant_group_permission(self):
         rest = _RestProxyForTest()
-        groups = Groups(rest)
+        permissions = Permissions(rest)
 
         expected_data = {}
         def check_input(data_dict):
             self.assertEqual(expected_data, data_dict)
             return True
-            
-        expected_data = {'member_id': "aaa", 'member_type': "USER"}
-        rest.expect_post("/group/g1/member", check_input, 200, {})
-        groups.add_user_to_group('aaa', 'g1')
-            
-        expected_data = {'member_id': "bbb", 'member_type': "GROUP"}
-        rest.expect_post("/group/g1/member", check_input, 200, {})
-        groups.add_group_to_group('bbb', 'g1')
-    
-    def test_delete_group_member(self):
-        rest = _RestProxyForTest()
-        groups = Groups(rest)
-            
-        rest.expect_delete("/group/g1/member?member_id=aaa&member_type=USER", 204)
-        groups.remove_user_from_group('aaa', 'g1')
-            
-        rest.expect_delete("/group/g1/member?member_id=aaa&member_type=GROUP", 204)
-        groups.remove_group_from_group('aaa', 'g1')
-            
-        rest.expect_delete("/group/g1/member?member_id=aaa&member_type=USER", 404)
+        
+        expected_data = {'right': 'ADMIN', 'target_type': 'PROJECT', 'target_id': 'id'}
+        rest.expect_post('/group_permission/g1', check_input, 200, expected_data)
+        
+        p1 = Permission('ADMIN', 'PROJECT', 'id')
+        permissions.grant_group_permission('g1', p1)
+        
+        rest.expect_post('/group_permission/g1', check_input, 404, {})
         with self.assertRaises(RuntimeError):
-            groups.remove_user_from_group('aaa', 'g1')
+            permissions.grant_group_permission('g1', p1)
+        
+    def test_revoke_user_permission(self):
+        rest = _RestProxyForTest()
+        permissions = Permissions(rest)
+        
+        rest.expect_delete(
+            '/user_permission/u1?right=ADMIN&target_type=PROJECT&target_id=id', 204)
             
-        rest.expect_delete("/group/g1/member?member_id=aaa&member_type=GROUP", 404)
+        p1 = Permission('ADMIN', 'PROJECT', 'id')
+        permissions.revoke_user_permission('u1', p1)
+        
+        rest.expect_delete(
+            '/user_permission/u1?right=ADMIN&target_type=PROJECT&target_id=id', 200)
+            
         with self.assertRaises(RuntimeError):
-            groups.remove_group_from_group('aaa', 'g1')
+            permissions.revoke_user_permission('u1', p1)
         
-    def test_group_memberships(self):
+    def test_revoke_group_permission(self):
         rest = _RestProxyForTest()
-        groups = Groups(rest)
+        permissions = Permissions(rest)
         
-        rest.expect_get('/group_membership?recursive=true&expand=true&group_uuid=g1', 200, 
-            {'items': []})
-        memberships = groups.get_group_memberships('g1')
-        self.assertTrue(len(memberships) == 0)
+        rest.expect_delete(
+            '/group_permission/g1?right=ADMIN&target_type=PROJECT&target_id=id', 204)
+            
+        p1 = Permission('ADMIN', 'PROJECT', 'id')
+        permissions.revoke_group_permission('g1', p1)
         
-        rest.expect_get('/group_membership?recursive=true&expand=true&group_uuid=g1', 200, 
-            {'items': [{'uuid': 'g2', 'name': 'n', 'description': 'd'}, {'uuid': 'g3'}]})
-        memberships = groups.get_group_memberships('g1')
-        self.assertTrue(len(memberships) == 2)
-        self.assertTrue('g2' in [g.get_uuid() for g in memberships])
-        self.assertTrue('g3' in [g.get_uuid() for g in memberships])
-        
-        rest.expect_get('/group_membership?recursive=true&expand=true', 200, 
-            {'items': [{'uuid': 'g2', 'name': 'n', 'description': 'd'}, {'uuid': 'g3'}]})
-        memberships = groups.get_my_memberships()
-        self.assertTrue(len(memberships) == 2)
-        self.assertTrue('g2' in [g.get_uuid() for g in memberships])
-        self.assertTrue('g3' in [g.get_uuid() for g in memberships])
-    
-    def test_group_members(self):
+        rest.expect_delete(
+            '/group_permission/g1?right=ADMIN&target_type=PROJECT&target_id=id', 200)
+            
+        with self.assertRaises(RuntimeError):
+            permissions.revoke_group_permission('g1', p1)
+            
+    def test_get_my_permissions(self):
         rest = _RestProxyForTest()
-        groups = Groups(rest)
+        permissions = Permissions(rest)
         
-        rest.expect_get('/group/g1/member', 200, {'items': []})
-        members = groups.get_group_members('g1')
-        self.assertTrue(len(members) == 0)
+        rest.expect_get('/user_permission?recursive=true', 200,
+            {'': [{'right': 'ADMIN', 'target_type': 'PROJECT', 'target_id': 'id'}]})
         
-        rest.expect_get('/group/g1/member', 200,
-            {'items': [{'member_id': 'u1', 'member_type': 'USER'},
-                       {'member_id': 'g2', 'member_type': 'GROUP'}]})
-        members = groups.get_group_members('g1')
-        self.assertTrue(len(members) == 2)
-        self.assertTrue('u1' in [g.get_id() for g in members if g.get_type() == 'USER'])
-        self.assertTrue('g2' in [g.get_id() for g in members if g.get_type() == 'GROUP'])
+        ps = permissions.get_my_permissions()
+        self.assertEqual(ps, {'': [Permission('ADMIN', 'PROJECT', 'id')]})
+            
+    def test_get_group_permissions(self):
+        rest = _RestProxyForTest()
+        permissions = Permissions(rest)
+        
+        rest.expect_get('/group_permission/g1?recursive=true', 200,
+            {'g1': [{'right': 'ADMIN', 'target_type': 'PROJECT', 'target_id': 'id'}],
+             'g2': [{'right': 'READ', 'target_type': 'GROUP', 'target_id': 'id2'}]})
+        
+        ps = permissions.get_group_permissions('g1')
+        self.assertEqual(ps, {'g1': [Permission('ADMIN', 'PROJECT', 'id')],
+                              'g2': [Permission('READ', 'GROUP', 'id2')]})
 
 
 if __name__ == '__main__':
