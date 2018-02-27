3e435bd65cb7b2bc0e5c5cff6c31aadb0fd80b7a
Laura Lark <laurahlark@gmail.com>
2018-02-26 19:18:00 -0500

    Add tests for config manager

--- adam/config_manager.py
+++ adam/config_manager.py
@@ -4,6 +4,9 @@ class Config(object):
     def __init__(self, config):
         self.config = config
     
+    def get_environment(self):
+        return self.config['env']
+    
     def get_url(self):
         return self.config['url']
     
@@ -20,19 +23,30 @@ class Config(object):
         self.config['workspace'] = workspace
 
 class ConfigManager(object):
-    def __init__(self, file_name):
-        with open(file_name, 'r') as f:
-            self.raw_config = json.load(f)
+    def __init__(self, file_name, raw_config=None):
+        """ Initializes from the given file. If a file name is not given, checks raw_config, where it would expect a python dictionary as would be parsed using json
+            from the config file.
+        """
+        if file_name is not None:
+            with open(file_name, 'r') as f:
+                self.raw_config = json.load(f)
+        elif raw_config is not None:
+            self.raw_config = raw_config
     
     def get_config(self, environment=None):
         if environment is None:
-            environment = self.raw_config['default_env']
+            if 'default_env' in self.raw_config:
+                environment = self.raw_config['default_env']
+        
+        if environment is None:
+            print('Error: no environment given, and no default environement specified.')
+            return None
         
         for config in self.raw_config['env_configs']:
             if config['env'] == environment:
                 return Config(config)
                 
-        print("Could not find config for environment %s" % (environment))
+        print("Error: could not find config for environment %s" % (environment))
         return None
 
     def to_file(self, file_name):
