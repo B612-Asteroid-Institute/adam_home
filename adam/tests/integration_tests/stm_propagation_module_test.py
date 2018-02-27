e84510c29a276c07788f8bb4986748276baab1fa
Laura Lark <laurahlark@gmail.com>
2018-02-27 14:47:18 -0500

    Use test_config.json instead of config.json to make it less painful to run tests with test config locally.

--- adam/tests/integration_tests/stm_propagation_module_test.py
+++ adam/tests/integration_tests/stm_propagation_module_test.py
@@ -16,7 +16,7 @@ class StmPropagationModuleTest(unittest.TestCase):
     
     """
     def setUp(self):
-        config = ConfigManager(os.getcwd() + '/config.json').get_config()
+        config = ConfigManager(os.getcwd() + '/test_config.json').get_config()
         self.service = Service(config)
         self.assertTrue(self.service.setup())
         self.working_project = self.service.new_working_project()
