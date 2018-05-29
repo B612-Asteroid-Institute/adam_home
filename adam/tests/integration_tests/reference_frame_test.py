e0119b5f3e6b8556ce8c79f1d486fb5bfd60db40
Laura Lark <laurahlark@gmail.com>
2018-05-29 17:44:13 -0400

    Lint

--- adam/tests/integration_tests/reference_frame_test.py
+++ adam/tests/integration_tests/reference_frame_test.py
@@ -6,7 +6,6 @@ from adam import BatchRunManager
 from adam import ConfigManager
 
 import unittest
-import datetime
 import os
 
 import numpy as np
@@ -19,7 +18,8 @@ class ReferenceFrameTest(unittest.TestCase):
     """
 
     def setUp(self):
-        config = ConfigManager(os.getcwd() + '/test_config.json').get_config('dev')
+        config = ConfigManager(
+            os.getcwd() + '/test_config.json').get_config('dev')
         self.service = Service(config)
         self.assertTrue(self.service.setup())
         self.working_project = self.service.new_working_project()
@@ -34,8 +34,8 @@ class ReferenceFrameTest(unittest.TestCase):
         start_time_str = "2000-01-01T11:58:55.816Z"
         end_time_str = "2009-07-21T21:55:08.813Z"
 
-        sun_icrf_state_vec =   [-306536341.5010222, -110979556.84640282, -48129706.42252728,
-                                15.75985527640906, -10.587567329195842, -4.589673432886975]
+        sun_icrf_state_vec = [-306536341.5010222, -110979556.84640282, -48129706.42252728,
+                              15.75985527640906, -10.587567329195842, -4.589673432886975]
 
         propagation_params = PropagationParams({
             'start_time': start_time_str,
@@ -55,8 +55,8 @@ class ReferenceFrameTest(unittest.TestCase):
         runner = BatchRunManager(self.service.get_batches_module(), [batch])
         runner.run()
         end_state = batch.get_results().get_end_state_vector()
-        expected_end_state = [73978163.61069362, -121822760.05571477, -52811158.83249758, 
-                            31.71000343989318, 29.9657246374751, .6754531613947713]
+        expected_end_state = [73978163.61069362, -121822760.05571477, -52811158.83249758,
+                              31.71000343989318, 29.9657246374751, .6754531613947713]
 
         difference = np.subtract(expected_end_state, end_state)
         print("Difference is %s" % difference)
@@ -64,14 +64,15 @@ class ReferenceFrameTest(unittest.TestCase):
         npt.assert_allclose(difference[0:3], [0, 0, 0], rtol=0, atol=.02)
         npt.assert_allclose(difference[3:6], [0, 0, 0], rtol=0, atol=.00002)
 
-        self.assertTrue("ICRF" in batch.get_results().get_parts()[-1].get_ephemeris())
+        ephem = batch.get_results().get_parts()
+        self.assertTrue("ICRF" in ephem)
 
     def test_sun_ememe(self):
         start_time_str = "2000-01-01T11:58:55.816Z"
         end_time_str = "2009-07-21T21:55:08.813Z"
 
         sun_ememe_state_vec = [-306536346.18024945, -120966638.54521248, -12981.069369263947,
-                                15.759854830195243, -11.539570959741736, 0.0005481049628786039]
+                               15.759854830195243, -11.539570959741736, 0.0005481049628786039]
 
         propagation_params = PropagationParams({
             'start_time': start_time_str,
@@ -93,7 +94,7 @@ class ReferenceFrameTest(unittest.TestCase):
         runner.run()
         end_state = batch.get_results().get_end_state_vector()
         expected_end_state = [73978158.47632701, -132777272.5255892, 5015.073123970032,
-                            31.710003506237434, 27.761693311026138, -11.299967713192564]
+                              31.710003506237434, 27.761693311026138, -11.299967713192564]
 
         difference = np.subtract(expected_end_state, end_state)
         print("Difference is %s" % difference)
@@ -104,8 +105,9 @@ class ReferenceFrameTest(unittest.TestCase):
         # THIS IS VERY STRANGE, BUT A FACT.
         # The values returned are actually in Sun-centered EMEME, but the ephemeris
         # reports that it is in Sun-centered ICRF.
-        self.assertTrue("ICRF" in batch.get_results().get_parts()[-1].get_ephemeris())
-        self.assertFalse("EMEME" in batch.get_results().get_parts()[-1].get_ephemeris())
+        ephem = batch.get_results().get_parts()[-1].get_ephemeris()
+        self.assertTrue("ICRF" in ephem)
+        self.assertFalse("EMEME" in ephem)
 
 
 if __name__ == '__main__':
