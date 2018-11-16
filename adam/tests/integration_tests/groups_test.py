41868de58264b5af8896243c83afeeb498184c03
EmmieKing <37819033+EmmieKing@users.noreply.github.com>
2018-11-16 11:24:23 -0800

    update adam_config.json path

--- adam/tests/integration_tests/groups_test.py
+++ adam/tests/integration_tests/groups_test.py
@@ -22,7 +22,7 @@ class GroupsTest(unittest.TestCase):
                 pass  # No big deal. Probably it was already deleted.
 
     def setUp(self):
-        config = ConfigManager(os.getcwd() + '/test_config.json').get_config()
+        config = ConfigManager(os.getcwd() + '/config/adam_config.json').get_config()
         self.service = Service(config)
         self.assertTrue(self.service.setup())
         self.me = "b612.adam.test@gmail.com"
