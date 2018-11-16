fad1b61262622e4154a146ce4919a841127feafa
EmmieKing <37819033+EmmieKing@users.noreply.github.com>
2018-11-16 11:52:29 -0800

    prepare for new test_adam_config.json file

--- adam/tests/integration_tests/backwards_propagation_test.py
+++ adam/tests/integration_tests/backwards_propagation_test.py
@@ -19,7 +19,7 @@ class BackwardsPropagationTest(unittest.TestCase):
     """
 
     def setUp(self):
-        config = ConfigManager(os.getcwd() + '/config/adam_config.json').get_config()
+        config = ConfigManager(os.getcwd() + '/test_adam_config.json').get_config()
         self.service = Service(config)
         self.assertTrue(self.service.setup())
         self.working_project = self.service.new_working_project()
