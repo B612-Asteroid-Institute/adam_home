080c5818d7b25a54380de7a6c84e34ada3390687
Carise Fernandez <carise@gmail.com>
2020-10-29 20:43:51 -0700

    Fix unit tests

--- adam/tests/stk/io_test.py
+++ adam/tests/stk/io_test.py
@@ -30,7 +30,7 @@ class StkIoTest(unittest.TestCase):
     def test_ephemeris_file_data_to_dataframe(self):
         ephemeris = ephemeris_file_data_to_dataframe(STK_EPHEM_TEXT.splitlines())
         self.assertEqual((4, 7), ephemeris.shape)
-        self.assertEquals(numpy.datetime64('2008-12-30T01:14:17.62'), ephemeris.values[0][0])
+        self.assertEqual(numpy.datetime64('2008-12-30T01:14:17.62'), ephemeris.values[0][0])
         self.assertAlmostEqual(9.361973576452e+7, ephemeris.values[0][1], 5)
         self.assertAlmostEqual(-198421940.32336992, ephemeris.values[0][2], 5)
         self.assertAlmostEqual(-2294415.6265469044, ephemeris.values[0][3], 5)
