ca588ffdea88234e227c42a79f6708f87224d2d6
Carise Fernandez <carise@gmail.com>
2021-02-02 18:14:55 -0800

    Address some flake8 stuff in adam.stk module

--- adam/stk/astrogatoroutput.py
+++ adam/stk/astrogatoroutput.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 class AstrogatorOutput:
     """Fetches output from an STK/Astrogator MCS.
-    
+
     This class is used to create a list of desired outputs from an
     STK Astrogator Mission Control Sequence (MCS).
     This is specifically designed to support multiple runs, such as
@@ -32,7 +32,6 @@ class AstrogatorOutput:
     +----------+-------+-------+-------+-------+-------+
     |   ...                                            |
     +----------+-------+-------+-------+-------+-------+
-
 
     NOTE: Internally the class stores the runs as rows, and the
     desired output values as columns. But the supported workflow
@@ -69,26 +68,26 @@ class AstrogatorOutput:
         self._df = None
 
     def add(self, eventName, quantity, unit):
-        """Add a new desired output 
-        
-        This function adds a desired output to the list of 
+        """Add a new desired output
+
+        This function adds a desired output to the list of
         outputs that will be generated when this object is updated.
-        It uses the MCS segment name, the desired Astroagtor 
-        CalcObject with which to calulate the value, and the 
+        It uses the MCS segment name, the desired Astroagtor
+        CalcObject with which to calulate the value, and the
         desired unit.
-        
+
         Args:
-            eventName (str): The MCS segment name 
+            eventName (str): The MCS segment name
                              (e.g., "Initial_State")
-            quantity (str): The desired CalcObject 
+            quantity (str): The desired CalcObject
                            (e.g., "Altitude")
-            unit (str): The desired unit in STK format 
+            unit (str): The desired unit in STK format
                         (e.g., "km")
-        
+
         Returns:
             None
-            
-         TODO: 
+
+         TODO:
             * Catch erros
             * Decide what to do if add is called after update()
 
@@ -97,22 +96,22 @@ class AstrogatorOutput:
 
     def update(self, gatorDefn, index):
         """Fetch & store the desired data from STK/Astrogator
-        
+
         This method fetches the desired output values from the
         STK/AStrogator Satellite Object.
-        
+
         Args:
             gatorDefn (STK COM object): The Astrogator definition of
                                         the STK Satellite Object.
             index: The run number, designed to be used in a loop that
                    increments each time.
-                   
-        TODO: 
+
+        TODO:
             * Catch error if index values called out of sequence (Until
               the next TODO is implemented)
             * Add remove need for an index, and so just always append
               the new run
-            
+
         """
         tempArray = np.zeros([1, len(self.events)])
         for i, eventRow in enumerate(self.events):
@@ -125,8 +124,8 @@ class AstrogatorOutput:
                         seg.GetResultValue(eventRow[1]).Format(eventRow[2]).value)
                 else:
                     print(
-                        'Format "{}" not supported (must be representable as a float). Substituting with "JDate".'.format(
-                            eventRow[2]))
+                        f'Format "{eventRow[2]}" not supported (must be representable as a float).'
+                        f' Substituting with "JDate".')
                     tempArray[0, i] = float(seg.GetResultValue(eventRow[1]).Format('JDate').value)
             else:
                 tempArray[0, i] = seg.GetResultValue(eventRow[1]).Getin(eventRow[2]).value
@@ -140,59 +139,59 @@ class AstrogatorOutput:
 
     def saveCSV(self, path):
         """Save the output values to a csv file
-        
+
         This uses the pandas dataframe funcationality to save the data.
         A csv file is used to support the workflow that a user may
         want to import into a spreadsheet.
-        
+
         Note: The data internally is not the same as the csv; Each
         desired output is a row in the csv, and each run is a column.
         Since this is the opposite of internal storage, the transpose
         function is used.
-        
+
         Args:
             path (str): The filepath to the output csv file,
                         including the suffix, ".csv"
-                        
+
         Returns:
             None:
-            
-         TODO: 
+
+         TODO:
             * Catch erros
-            
+
         """
         self.df.transpose().to_csv(path)
 
     @classmethod
     def loadCSV(cls, path):
         """ Load the data from a csv file
-        
+
         This method loads data into the object from a csv file.
         It is inteneded to load a csv that was created with this
-        same object. 
-        
+        same object.
+
         Args:
             path (str): The filepath to the output csv file,
                         including the suffix, ".csv"
-                        
+
         Returns:
             The class
 
-        TODO: 
+        TODO:
             * Catch erros
-        
+
         """
         c = cls()
         c._df = pd.read_csv(path, index_col=0)
         c.results = c._df.as_matrix()
         c._rowNames = c._df.index.values.tolist()
 
-        # Split the event name into its three components, which were 
+        # Split the event name into its three components, which were
         # used to add the event in the first place. The three
         # components that make up the event name are:
         #  1) MCS Segment Name
         #  2) Desired CalcObject quantity
-        #  3) Units 
+        #  3) Units
         c.events = list(c.df.index.map(lambda x: x.replace(')', '')).map(
             lambda x: x.replace('(', '')).str.split(" ").values)
         return c
@@ -200,21 +199,21 @@ class AstrogatorOutput:
     @property
     def rowNames(self):
         """Return the names of the row as a list
-        
+
         This creates the row name from the event name, the desired
         quantity, and the units. Put the untis in parenthesis
         which is common for human-readable tables.
-        
+
         Args:
             None
-            
+
         Returns:
             List of row names
-            
+
         TODO:
             * Make this work even if add() has been called since the
               last time rowNames() was called.
-              
+
         """
         if self._rowNames is None:
             self._rowNames = []
@@ -226,15 +225,15 @@ class AstrogatorOutput:
     @property
     def df(self):
         """Return a pandas dataframe of the data
-        
+
         Note: The data internally is not the same as the returned
-        dataframe; Eachdesired output is a row in the dataframe, and 
-        each run is a column. Since this is the opposite of internal 
+        dataframe; Eachdesired output is a row in the dataframe, and
+        each run is a column. Since this is the opposite of internal
         storage, the transpose function is used.
-                
+
         Args:
             None
-            
+
         Returns:
             Panda data frame
         """
